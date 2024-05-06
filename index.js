f63400f76a1161975928b1eb68e378103564997a
Gurushesh Pawar <62001513+gurupawar@users.noreply.github.com>
2024-05-06 09:29:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,7 @@ function ignoreImports(state, startLine, endLine, silent) {
     pos = state.bMarks[startLine] + state.tShift[startLine],
     max = state.eMarks[startLine];
 
-  // Check if the line starts with "import"
   if (state.src.substr(pos, 6) === "import") {
-    // if (!silent) {
-    //   console.log("Removed import line:", state.src.slice(pos, max));
-    // }
-    // Skip the line
     state.line++;
     return true;
   }
