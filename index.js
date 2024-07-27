b2a7863735ade8d5c43e08cc9dc1a2363150d5a4
gurupawar <g.r.pawar000@gmail.com>
2024-07-27 15:13:26 +0530

    removed commented code

--- index.js
+++ index.js
@@ -1,34 +1,12 @@
-// "use strict";
-
-// function ignoreImports(state, startLine, endLine, silent) {
-//   let pos = state.bMarks[startLine] + state.tShift[startLine];
-//   const max = state.eMarks[startLine];
-
-//   // Check if the line starts with "import"
-//   if (max - pos >= 6 && state.src.slice(pos, pos + 6) === "import") {
-//     if (silent) return true; // Don't run rule, just report match
-//     state.line = startLine + 1;
-//     return true;
-//   }
-
-//   return false;
-// }
-
-// module.exports = function ignore_imports_plugin(md) {
-//   md.block.ruler.before("paragraph", "ignoreimports", ignoreImports);
-// };
-
 "use strict";
 
 function ignoreImports(state, startLine, endLine, silent) {
   let pos = state.bMarks[startLine] + state.tShift[startLine];
   const max = state.eMarks[startLine];
 
-  // Check if the line starts with "import"
   if (max - pos >= 6 && state.src.slice(pos, pos + 6) === "import") {
-    if (silent) return true; // Don't run rule, just report match
+    if (silent) return true;
 
-    // Skip this line
     state.line = startLine + 1;
     return true;
   }
